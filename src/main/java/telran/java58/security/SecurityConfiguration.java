8986268ae79c9ce535b64d845cf5ad22c27c2db1
edward <edwardlfish@gmail.com>
2025-07-21 17:18:43 +0300

    Authorization for add post and add comment

--- src/main/java/telran/java58/security/SecurityConfiguration.java
+++ src/main/java/telran/java58/security/SecurityConfiguration.java
@@ -23,10 +23,12 @@ public class SecurityConfiguration {
                     .permitAll()
                 .requestMatchers("/account/user/{login}/role/{role}")
                     .hasRole(Role.ADMINISTRATOR.name())
-                .requestMatchers(HttpMethod.PATCH, "/account/user/{login}")
+                .requestMatchers(HttpMethod.PATCH, "/account/user/{login}", "/forum/post/{id}/comment/{login}")
                     .access(new WebExpressionAuthorizationManager("#login == authentication.name"))
                 .requestMatchers(HttpMethod.DELETE, "/account/user/{login}")
                     .access(new WebExpressionAuthorizationManager("#login == authentication.name or hasRole('ADMINISTRATOR')"))
+                .requestMatchers(HttpMethod.POST, "/forum/post/{author}")
+                    .access(new WebExpressionAuthorizationManager("#author == authentication.name"))
                 .anyRequest()
                     .authenticated()
         );
